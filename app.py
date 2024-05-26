da999fac1174e64331a14ba6e401a1930a9ad493
carlosta <120747242+Carlosta1177@users.noreply.github.com>
2024-05-26 12:24:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -193,8 +193,8 @@ if 'ruta_turistica' in st.session_state:
         
         folium.GeoJson(ruta_geojson, name='route').add_to(m)
         folium.LayerControl().add_to(m)
-        
-        st_folium(m, width=700, height=500)
+        with st.form(key='myform'):
+            st_folium(m, width=700, height=500)
 
 # Mostrar lista de fallas ordenadas por distancia según el tipo y la categoría seleccionados
 if 'ubicacion_usuario' in st.session_state:
