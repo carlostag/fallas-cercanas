35457db844e03fe0300d2b92d8c366c1bb575b65
carlosta <120747242+Carlosta1177@users.noreply.github.com>
2024-05-26 12:48:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -187,10 +187,10 @@ if 'ruta_turistica' in st.session_state:
         # Mostrar mapa con la ruta
         m = folium.Map(location=ubicacion_usuario, zoom_start=14)
         folium.Marker([ubicacion_usuario[0], ubicacion_usuario[1]], popup="Tu Ubicación", icon=folium.Icon(color="blue")).add_to(m)
-        print('aqui')
+
         for index, row in ruta_turistica.iterrows():
             folium.Marker([row['geo_point_2d_lat'], row['geo_point_2d_lon']], popup=row['Nom / Nombre']).add_to(m)
-        print('aqui')
+
         folium.GeoJson(ruta_geojson, name='route').add_to(m)
         folium.LayerControl().add_to(m)
         st_folium(m, width=700, height=500)
